43c9511520a9c6be06e208cbac3f3e652f7bca80
Adityajaiswal11 <157581467+Adityajaiswal11@users.noreply.github.com>
2024-04-07 11:37:57 +0530

    Added comment script.js

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
+// Selects the HTML element with the class "container" and assigns it to the variable container
 const container = document.querySelector(".container");
 
+// Adds a click event listener to the HTML element with the class "open-navbar-icon"
+// When clicked, it adds the class "change" to the container element
 document.querySelector(".open-navbar-icon").addEventListener("click", () => {
   container.classList.add("change");
 });
 
+// Adds a click event listener to the HTML element with the class "close-navbar-icon"
+// When clicked, it removes the class "change" from the container element
 document.querySelector(".close-navbar-icon").addEventListener("click", () => {
   container.classList.remove("change");
 });
 
-const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
 
-let i = 0;
-
-Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
-  item.style.cssText = `background-color: ${colors[i++]}`;
-});
-
-Array.from(document.querySelectorAll(".navigation-button")).forEach(item => {
-  item.onclick = () => {
-    item.parentElement.parentElement.classList.toggle("change");
-  };
-});
