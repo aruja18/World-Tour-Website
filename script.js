93a53214dd18e8fc03b02bb34deb2e3f01e0ff8b
Adityajaiswal11 <157581467+Adityajaiswal11@users.noreply.github.com>
2024-04-10 11:45:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// Selects the HTML element with the class "container" and assigns it to the variable containers
+// Selects the HTML element with the class "container" and assigns it to the variable container
 const container = document.querySelector(".container");
 
 // Adds a click event listener to the HTML element with the class "open-navbar-icon"
-// When clicked, it adds the class "change" to the container elements
+// When clicked, it adds the class "change" to the container element
 document.querySelector(".open-navbar-icon").addEventListener("click", () => {
   container.classList.add("change");
 });
 
 // Adds a click event listener to the HTML element with the class "close-navbar-icon"
-// When clicked, it removes the class "change" from the container elements
+// When clicked, it removes the class "change" from the container element
 document.querySelector(".close-navbar-icon").addEventListener("click", () => {
   container.classList.remove("change");
 });
