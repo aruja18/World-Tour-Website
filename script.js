5753c4ff9ef3a8322ae6592d7c1646aaa7ecf4e0
Adityajaiswal11 <157581467+Adityajaiswal11@users.noreply.github.com>
2024-04-10 11:43:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 const container = document.querySelector(".container");
 
 // Adds a click event listener to the HTML element with the class "open-navbar-icon"
-// When clicked, it adds the class "change" to the container element
+// When clicked, it adds the class "change" to the container elements
 document.querySelector(".open-navbar-icon").addEventListener("click", () => {
   container.classList.add("change");
 });
 
 // Adds a click event listener to the HTML element with the class "close-navbar-icon"
-// When clicked, it removes the class "change" from the container elements
+// When clicked, it removes the class "change" from the container element
 document.querySelector(".close-navbar-icon").addEventListener("click", () => {
   container.classList.remove("change");
 });
@@ -19,7 +19,7 @@ const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
 let i = 0;
 
 // Loops through each HTML element with the class "nav-link"
-// Assigns a background color from the colors array to each element
+// Assigns a background color from the colors array to each elements
 Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
   item.style.cssText = `background-color: ${colors[i++]}`;
 });
@@ -28,12 +28,12 @@ const homeLink = document.querySelector('.nav-list a:first-child');
 homeLink.addEventListener('click', () => {
   // Redirects to the main page (replace 'index.html' with the name of your main HTML file)
   window.location.href = 'index.html';
-  // Closes the navbar
+  // Closes the navbars
   container.classList.remove("change");
 });
 
-// Loops through each HTML element with the classes "navigation-button"
-// Toggles the class "change" on the parent element of the clicked button's parent element
+// Loops through each HTML element with the classes "navigation-buttons"
+// Toggles the class "change" on the parent element of the clicked button's parent elements
 Array.from(document.querySelectorAll(".navigation-button")).forEach(item => {
   item.onclick = () => {
     item.parentElement.parentElement.classList.toggle("change");
