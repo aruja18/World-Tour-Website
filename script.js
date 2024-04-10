923d501dea4a15d7d99d7a16461ad4390742f677
Adityajaiswal11 <157581467+Adityajaiswal11@users.noreply.github.com>
2024-04-10 11:51:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
 // Initializes a variable i with value 0
 let i = 0;
 
-// Loops through each HTML element with the class "nav-link"
-// Assigns a background color from the colors array to each elements
+// Loops through each HTML element with the class "nav-links"
+// Assigns a background color from the colors array to each element
 Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
   item.style.cssText = `background-color: ${colors[i++]}`;
 });
@@ -32,7 +32,7 @@ homeLink.addEventListener('click', () => {
   container.classList.remove("change");
 });
 
-// Loops through each HTML element with the classes "navigation-button"
+// Loops through each HTML element with the classes "navigation-buttons"
 // Toggles the class "change" on the parent element of the clicked button's parent element
 Array.from(document.querySelectorAll(".navigation-button")).forEach(item => {
   item.onclick = () => {
